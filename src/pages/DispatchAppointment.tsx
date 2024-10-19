08b9b22f496a46ae094d07f57662d54526ba5db1
Nguyen Duy Hung <hungndse184681@fpt.edu.vn>
2024-10-19 11:26:08 +0700

    Update DispatchAppointment.tsx

--- src/pages/DispatchAppointment.tsx
+++ src/pages/DispatchAppointment.tsx
@@ -1,6 +1,3 @@
-import React, { useContext } from 'react';
-
-import { AuthProvider } from '../hooks/context/AuthContext';
 import { useAuth } from '../hooks/context/AuthContext';
 import StaffAppointment from './StaffAppointment';
 import CustomerAppointment from './CustomerAppointment';
@@ -10,16 +7,45 @@ interface User {
     userId: number;
 }
 
-const MyAppointment = () => {
+const DispatchAppointment = () => {
     const { user } = useAuth();
 
     if (user?.roleId === 'STA') {
         return <StaffAppointment />;
+
     } else if (user?.roleId === 'CUS') {
         return <CustomerAppointment />;
+        
     } else {
         return <div>Bạn không có quyền truy cập trang này.</div>;
     }
 };
 
-export default MyAppointment;
+export default DispatchAppointment;
+
+
+// import { useContext } from 'react';
+// import  AuthContext  from "../hooks/context/AuthContext";
+// import CustomerAppointment from "./CustomerAppointment";
+// import StaffAppointment from "./StaffAppointment";
+// import UnauthorizedPage from "./UnauthorizedPage";
+
+// interface User {
+//     roleId: string;
+//     userId: number;
+// }
+
+// const MyAppointmentPage = () => {
+//     const { user } = useContext(AuthContext);
+
+//     // Check the user's role and render the corresponding appointment page
+//     if (user?.role === 'CUS') {
+//         return <CustomerAppointment />;
+//     } else if (user?.role === 'STA') {
+//         return <StaffAppointment />;
+//     } else {
+//         return <UnauthorizedPage />;
+//     }
+// };
+
+// export default MyAppointmentPage;
