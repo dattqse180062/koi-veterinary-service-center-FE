ec8c779452966e4540a450795d365cdd687e02af
Nguyen Duy Hung <hungndse184681@fpt.edu.vn>
2024-10-20 12:19:44 +0700

    Update feedbackApi.ts

--- src/api/feedbackApi.ts
+++ src/api/feedbackApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/api/v1/feedbacks/all";
 const API_BASE_URL = "http://localhost:8080/api/v1/feedbacks";
 
-// Function to fetch feedback
+// Function to fetch feedback for manager
 export const fecthFeedbacks = async () => {
   try {
     const response = await axios.get(API_URL);
@@ -14,10 +14,36 @@ export const fecthFeedbacks = async () => {
   }
 };
 
-// Function to get feedback details information
+// Function to get feedback details information for manager
 export const fecthFeedbackDetails = async (feedback_id: number): Promise<any> => {
   try {
     const response = await axios.get(`${API_BASE_URL}/${feedback_id}`,
+      {
+        params: { feedback_id },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    throw error;
+  }
+};
+
+// For Veterinarian get feedbacks
+export const fetchVetFeedbacks = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/veterinarian`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching feedback:", error);
+    throw error;
+  }
+};
+
+// For Veterinarian get feedback details
+export const fetchVetFeedbackDetails = async (feedback_id: number): Promise<any> => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/${feedback_id}/veterinarian`,
       {
         params: { feedback_id },
       }
